1321208d2b37c5de223b942ab84c628e1e9ee713
Nguyen Louis <69016056+Azlou77@users.noreply.github.com>
2023-05-17 09:26:14 +0000

    add name pokemon

--- src/components/PokemonList/PokemonList.js
+++ src/components/PokemonList/PokemonList.js
@@ -1,14 +1,35 @@
-import React  from "react";
+import React, { useState, useEffect } from 'react';
 
-function PokemonList({ searchTerm = "" }) {
+// function PokemonList({ searchTerm = "" }) {
 
 
-  return (
-    <ul>
-    <li>test</li>
-    <li>test2</li>
-    </ul>
-  );
-}
+  function PokemonList() {
+    const [pokemonData, setPokemonData] = useState([]);
+  
+    useEffect(() => {
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=50')
+        .then((response) => response.json())
+        .then((data) => setPokemonData(data.results));
+    }, []);
+  
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {pokemonData.map((pokemon) => (
+            <tr key={pokemon.name}>
+              <td>{pokemon.name}</td>
+              <td>{pokemon.url}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
 
 export default PokemonList;
